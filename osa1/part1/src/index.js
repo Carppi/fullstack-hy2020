9981096e67ccb6280f8f35e634a72ad3b64f2a93
Juho <juho.carpen@gmail.com>
2021-01-02 12:14:18 +0200

    commitPart1
    Part 1 lisätty, jossa  työskennelty suoraan kurssin ohjeiden mukaan. Myös Kurssitiedot ohjelma on alustettu

---
+++ osa1/part1/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const nimi = 'Pekka'
+  const ika = 10
+  
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26+10} />
+      <Hello name={nimi} age={ika} />
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
