3bf92bd37683e679fd7a6b4a1f3a5919df165f76
Juho <juho.carpen@gmail.com>
2021-01-02 12:40:46 +0200

    Tehtävä 1.1 suoritettu

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -1,6 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>{props.partName} {props.exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.number1 + props.number2 + props.number3}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -12,17 +34,15 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+
+      <Header course={course} />
+
+      <Content partName={part1} exercises={exercises1} />
+      <Content partName={part2} exercises={exercises2} />
+      <Content partName={part3} exercises={exercises3} />
+
+      <Total number1={exercises1} number2={exercises2} number3={exercises3} />
+
     </div>
   )
 }
