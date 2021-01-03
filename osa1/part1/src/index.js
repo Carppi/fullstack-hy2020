ddc0eceacd139c7216d9d9afe5df899d5b2e6658
Juho <juho.carpen@gmail.com>
2021-01-03 12:31:00 +0200

    osa 1c

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,23 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>Hello {props.name}, you are {props.age} years old</p>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
     </div>
   )
 }
 
-const App = () => {
+
+//Counter-appin alikomponentteja, jotka tukevat sen toimintaa
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const CounterApp = (props) => {
+  const [counter, setCounter] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  console.log("Counter-appi alkaa tästä", counter)
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      />
+    </div>
+  )
+}
+
+const App = (props) => {
+
+  //nimi-appin muuttujat
   const nimi = 'Pekka'
   const ika = 10
-  
+
   return (
     <div>
       <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10} />
+      <Hello name="Maya" age={26 + 10} />
       <Hello name={nimi} age={ika} />
+      <h1>Counter-app</h1>
+      <div>{CounterApp()}</div>
     </div>
   )
 }
@@ -25,7 +72,7 @@ const App = () => {
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
