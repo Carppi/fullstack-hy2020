92d4e4278c32772192cc07cefcb73a0cc1ad9423
Juho <juho.carpen@gmail.com>
2021-01-04 17:43:19 +0200

    1.10 suoritettu

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,85 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Button = (props) => {
+  // console.log(props)
+  const { handleClick, text } = props
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  return (
+    < button onClick={handleClick} >
+      {text}
+    </button >
+  )
+}
+
+const StatisticsLine = (props) => {
+
+  const { text, value } = props
+
+  return (
+    <p>{text}: {value}</p>
+  )
+}
+
+const Statistics = (props) => {
+  
+  console.log(props)
+
+  const { good, neutral, bad } = props
+  const all = good + neutral + bad
+
+  if (all === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+
+  const average = (good - bad) / all
+  const positive = good / all * 100 + '%'
+  const [allStatistics, setStatistics] = useState([])
+
+  return (
+    <div>
+      <StatisticsLine text="Good" value={good} />
+      <StatisticsLine text="Neutral" value={neutral} />
+      <StatisticsLine text="Bad" value={bad} />
+      <StatisticsLine text="All" value={all} />
+      <StatisticsLine text="Average" value={average} />
+      <StatisticsLine text="Positive" value={positive} />
+    </div>
+  )
+
+}
+
+const App = () => {
+
+  //napit tallennettuna omiin tiloihinsa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' />
+      <h1>Statistics</h1>
+      <Statistics bad={bad} neutral={neutral} good={good} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
