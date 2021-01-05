b1789db1f70a26e2db83d5477cd29ace5e58f3ba
Juho <juho.carpen@gmail.com>
2021-01-05 21:08:19 +0200

    kopioitu pohjalle tehtävien 1 pohja

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,86 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+
+  const parts = props.parts
+
+  return (
+
+    <div>
+      { // alla testattuna loopia, joka ei kuitenkaan lopulta toiminut sijoitusvaiheessa
+      /* {parts.forEach(element => {
+        console.log(element);
+        console.log(element.name);
+        <Part partName={element.name} exerciseNumber={element.exercises} />
+      })} */}
+
+      <Part partName={parts[0].name} exerciseNumber={parts[0].exercises} />
+      <Part partName={parts[1].name} exerciseNumber={parts[1].exercises} />
+      <Part partName={parts[2].name} exerciseNumber={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.partName} {props.exerciseNumber}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+
+  const parts = props.parts
+  let numberOfExercises = 0
+
+  parts.forEach(element => {
+    numberOfExercises += element.exercises
+  });
+
+  return (
+    <p>Number of exercises {numberOfExercises}</p>
+  )
+}
+
+const App = () => {
+
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
