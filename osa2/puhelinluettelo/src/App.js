3a606144b56474eabc535e86992fae5ea4eaf2d5
Juho <juho.carpen@gmail.com>
2021-01-06 14:46:40 +0200

    t2.6 suoritettu. Nimiä voi lisätä

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' },
+    { name: 'Grace Hopper' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addName = (event) => {
+    console.log('submit button clicked', event.target)
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+      {persons.map((person) => {
+        console.log(person)
+        return <li key={person.name}>{person.name}</li>
+      })}
+      </ul>
+      <h2>debugging:</h2>
+      <div>debug: newName is {newName}</div>
+    </div>
+  )
+
+}
+
+export default App
