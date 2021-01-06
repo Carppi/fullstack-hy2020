1af75fad0d761185cbe206818ad0b044a4b579f6
Juho <juho.carpen@gmail.com>
2021-01-06 15:32:28 +0200

    t2.8. numeron lisääminen + tiedot taulukkomuotoon

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Grace Hopper' }
+    { name: 'Arto Hellas', number: '040-1231244' },
+    { name: 'Grace Hopper', number: '050-11122233' }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
-    
+
     const nameArray = persons.map(person => person.name)
-    
+
     //tarkista onko lisättävä nimi sovelluksen tiedossa
     if (nameArray.includes(newName)) {
       window.alert(`${newName} is already added to phonebook`) //jos nimi löytyy, anna virheilmoitus komennolla alert
-    } else { 
+    } else {
       //jos ei, lisää tiedostoon
       const personObject = {
-        name: newName
+        name: newName,
+        number: newNumber
       }
-  
+
       setPersons(persons.concat(personObject))
       setNewName('')
+      setNewNumber('')
     }
 
   }
@@ -31,25 +34,40 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-      {persons.map((person) => {
-        return <li key={person.name}>{person.name}</li>
-      })}
-      </ul>
-      <h2>debugging:</h2>
-      <div>debug: newName is {newName}</div>
+      <table>
+        <tbody>
+
+          {persons.map((person) => {
+            return (
+              <tr key={person.name}>
+                <td>{person.name}</td>
+                <td>{person.number}</td>
+              </tr>)
+          })}
+
+        </tbody>
+      </table>
+      {/* <h2>debugging:</h2>
+      <div>debug: newName is {newName}</div> */}
     </div>
   )
 
