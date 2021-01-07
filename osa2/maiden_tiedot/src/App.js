e25636b24a82b3fecc49c907975156edc848ad25
Juho <juho.carpen@gmail.com>
2021-01-07 15:32:58 +0200

    t2.12. Maiden perustiedot löytyy

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
-function App() {
+const Filter = ({ filterValue, filterFunction }) => (
+  <div>
+    find countries <input value={filterValue} onChange={filterFunction} />
+  </div>
+)
+
+const Countries = ({ countries, filter }) => {
+
+  const countriesToShow = (
+    countries.filter(country => (
+      country
+        .name.toLowerCase()
+        .includes(filter.toLowerCase())
+    )
+    )
+  )
+
+  if (countriesToShow.length > 10) {
+    return (
+      <div>Too many matches, specify another filter</div>
+    )
+  } else if (countriesToShow.length > 1) {
+    return (
+      <table>
+        <tbody>
+          {countriesToShow.map(country => (
+            <Country country={country} key={country.alpha3Code} />
+          ))}
+        </tbody>
+      </table>
+    )
+  } else if (countriesToShow.length === 1) {
+    return (
+      <CountryDetailed country={countriesToShow[0]} />
+    )
+  } else {
+    return (
+      <div>No matches, specify another filter</div>
+    )
+  }
+
+
+}
+
+const Country = ({ country }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <tr>
+      <td>
+        {country.name}
+      </td>
+    </tr>
+  )
+}
+
+const CountryDetailed = ({ country }) => {
+  
+  return (
+    <div>
+      <h2>{country.name}</h2>
+      <table>
+        <tbody>
+          <tr>
+            <td>capital</td>
+            <td>{country.capital}</td>
+          </tr>
+          <tr>
+            <td>population</td>
+            <td>{country.population}</td>
+          </tr>
+        </tbody>
+      </table>
+      <h3>languages</h3>
+      <ul>
+        {country.languages.map(language => (
+          <li key={language.iso639_2}>{language.name}</li>
+        ))}
+      </ul>
+      <img
+        src={country.flag}
+        alt={`Flag of ${country.name}`}
+        width="50%" height="50%"
+      />
+    </div>
+  )
+}
+
+const App = () => {
+
+  const [countries, setCountries] = useState([])
+  const [filter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleFilter = (event) => {
+    setNewFilter(event.target.value)
+  }
+
+  return (
+    <div>
+      <h1>Country finder</h1>
+      <Filter filterValue={filter} filterFunction={handleFilter} />
+      <Countries countries={countries} filter={filter} />
     </div>
   );
 }
