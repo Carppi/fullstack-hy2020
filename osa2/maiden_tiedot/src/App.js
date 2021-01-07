06720b776323762d6f27097d0859a3444e6bf813
Juho <juho.carpen@gmail.com>
2021-01-07 19:12:44 +0200

    t2.14. säätiedotus lisätty

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -9,6 +9,7 @@ const Filter = ({ filterValue, filterFunction }) => (
 
 const Countries = ({ countries, filter, setNewFilter }) => {
 
+  console.log('Countries kutsuttu')
   const countriesToShow = (
     countries.filter(country => (
       country
@@ -24,11 +25,13 @@ const Countries = ({ countries, filter, setNewFilter }) => {
     )
   } else if (countriesToShow.length > 1) {
     return (
-      <CountryList countriesToShow={countriesToShow} setNewFilter={setNewFilter}/>
+      <CountryList countriesToShow={countriesToShow} setNewFilter={setNewFilter} />
     )
   } else if (countriesToShow.length === 1) {
     return (
-      <CountryDetailed country={countriesToShow[0]} />
+      <CountryDetailed
+        country={countriesToShow[0]}
+      />
     )
   } else {
     return (
@@ -39,12 +42,12 @@ const Countries = ({ countries, filter, setNewFilter }) => {
 
 }
 
-const CountryList = ({countriesToShow, setNewFilter}) => {
+const CountryList = ({ countriesToShow, setNewFilter }) => {
   return (
     <table>
       <tbody>
         {countriesToShow.map(country => (
-          <CountryShort country={country} key={country.alpha3Code} setNewFilter={setNewFilter}/>
+          <CountryShort country={country} key={country.alpha3Code} setNewFilter={setNewFilter} />
         ))}
       </tbody>
     </table>
@@ -52,9 +55,9 @@ const CountryList = ({countriesToShow, setNewFilter}) => {
 }
 
 const CountryShort = ({ country, setNewFilter }) => {
-  
+
   const handleClick = () => {
-    console.log('Button clicked in ',country.name)
+    console.log('Button clicked in ', country.name)
     setNewFilter(country.name)
   }
 
@@ -72,8 +75,67 @@ const CountryShort = ({ country, setNewFilter }) => {
   )
 }
 
+//säätiedot https://weatherstack.com/ palvelusta
+const CityWeather = ({ cityName }) => {
+
+  console.log('CityWeather kutsuttu')
+  const [weather, setWeather] = useState([])
+
+  useEffect(() => {
+    console.log('effect is running')
+    let mounted = true
+    const api_key = process.env.REACT_APP_API_KEY
+    const params = {
+      access_key: api_key,
+      query: cityName,
+      units: 'm'
+    }
+
+    const getWeatherData = (params) => (
+      axios
+        .get('http://api.weatherstack.com/current', { params })
+        .then(response => {
+          //console.log('response', response)
+          const apiResponse = response.data
+          console.log(`Current temperature in ${apiResponse.location.name} is ${apiResponse.current.temperature}℃`)
+          if (mounted) {
+            setWeather(apiResponse)
+          }
+        }).catch(error => {
+          console.log(error)
+        })
+    )
+
+    getWeatherData(params)
+
+    return mounted = false
+  }, [cityName])
+
+  console.log('weather', weather)
+
+  if (weather.length === 0) {
+    return <div>Under construction</div>
+  } else {
+    return (
+      <div>
+        <h3>{`Weather in ${weather.location.name}, ${weather.location.country}`}</h3>
+        <p><b>Temperature: </b> {weather.current.temperature} Celsius</p>
+        <img
+          src={weather.current.weather_icons[0]}
+          alt={`${weather.current.weather_descriptions[0]}`}
+          width="25%" height="25%"
+        />
+        <p><b>Wind:</b> {weather.current.wind_speed} km/h {weather.current.wind_dir} </p>
+      </div>
+    )
+  }
+
+
+
+}
+
 const CountryDetailed = ({ country }) => {
-  
+  console.log('CountryDetailed kutsuttu')
   return (
     <div>
       <h2>{country.name}</h2>
@@ -89,7 +151,7 @@ const CountryDetailed = ({ country }) => {
           </tr>
         </tbody>
       </table>
-      <h3>languages</h3>
+      <h3>Spoken languages</h3>
       <ul>
         {country.languages.map(language => (
           <li key={language.iso639_2}>{language.name}</li>
@@ -100,6 +162,9 @@ const CountryDetailed = ({ country }) => {
         alt={`Flag of ${country.name}`}
         width="50%" height="50%"
       />
+      <CityWeather
+        cityName={country.capital}
+      />
     </div>
   )
 }
@@ -108,6 +173,7 @@ const App = () => {
 
   const [countries, setCountries] = useState([])
   const [filter, setNewFilter] = useState('')
+
 
   useEffect(() => {
     axios
@@ -125,7 +191,11 @@ const App = () => {
     <div>
       <h1>Country finder</h1>
       <Filter filterValue={filter} filterFunction={handleFilter} />
-      <Countries countries={countries} filter={filter} setNewFilter={setNewFilter}/>
+      <Countries
+        countries={countries}
+        filter={filter}
+        setNewFilter={setNewFilter}
+      />
     </div>
   );
 }
