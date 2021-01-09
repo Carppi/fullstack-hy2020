5e9c581817bbc9fb3bcd281edc7598319b00306e
Juho <juho.carpen@gmail.com>
2021-01-09 20:14:03 +0200

    t3.9. Frontend toimii backendin kanssa

--- osa3/puhelinluettelo/src/services/persons.js
+++ osa3/puhelinluettelo/src/services/persons.js
@@ -1,23 +1,27 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'http://localhost:3001/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
+    console.log('getAll onnistuu')
     return request.then(response => response.data)
 }
 
 const create = newObject => {
     const request = axios.post(baseUrl, newObject)
+    console.log('create onnistuu')
     return request.then(response => response.data)   
 }
 
 const deletePerson = (id) => {
     const request = axios.delete(`${baseUrl}/${id}`)
+    console.log('deletePerson onnistuu')
     return request.then(response => response.data)
 }
 
 const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`,newObject)
+    console.log('update onnistuu')
     return request.then(response => response.data)
 }
 
