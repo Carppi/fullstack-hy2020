cfab946ab5ac9f3d7ef4d792c6f5dcf785cb4009
Juho <juho.carpen@gmail.com>
2021-01-17 01:31:26 +0200

    4.10. HTTP POSTin testaus (refaktorointi aiemmin)

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -23,15 +23,15 @@ describe('HTTP GET request to /api/blogs', () => {
 
   test('all blogs are returned (i.e. blogcount is correct)', async () => {
     const response = await api.get('/api/blogs')
-  
+
     expect(response.body).toHaveLength(helper.initialBlogs.length)
   })
 
   test('a specific blog is within the returned blogs', async () => {
     const response = await api.get('/api/blogs')
-  
+
     const titles = response.body.map(r => r.title)
-  
+
     expect(titles).toContain(
       'Go To Statement Considered Harmful'
     )
@@ -42,6 +42,33 @@ describe('HTTP GET request to /api/blogs', () => {
     expect(response.body[0].id).toBeDefined()
   })
 
+})
+
+/* 
+Tee testi joka varmistaa että sovellukseen voi lisätä blogeja 
+osoitteeseen /api/blogs tapahtuvalla HTTP POST -pyynnölle. 
+Testaa ainakin, että blogien määrä kasvaa yhdellä. 
+Voit myös varmistaa, että oikeansisältöinen blogi on lisätty järjestelmään. */
+describe('HTTP POST request to /api/blogs', () => {
+
+  test('a valid blog can be added', async () => {
+    const newBlog = helper.newBlogs[0]
+
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const response = await api.get('/api/blogs')
+
+    const titles = response.body.map(r => r.title)
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    expect(titles).toContain(
+      'First class tests'
+    )
+  })
 })
 
 afterAll(() => {
--- osa4/blogilista/tests/test_helper.js
+++ osa4/blogilista/tests/test_helper.js
@@ -22,6 +22,34 @@ const initialBlogs = [
   }
 ]
 
+const newBlogs = [
+  {
+    _id: "5a422b891b54a676234d17fa",
+    title: "First class tests",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+    likes: 10,
+    __v: 0
+  },
+  {
+    _id: "5a422ba71b54a676234d17fb",
+    title: "TDD harms architecture",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+    likes: 0,
+    __v: 0
+  },
+  {
+    _id: "5a422bc61b54a676234d17fc",
+    title: "Type wars",
+    author: "Robert C. Martin",
+    url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+    likes: 2,
+    __v: 0
+  }
+]
+
 module.exports = {
-  initialBlogs
+  initialBlogs,
+  newBlogs
 }
