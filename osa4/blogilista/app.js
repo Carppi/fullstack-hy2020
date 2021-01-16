d099d45edad241121a845b04a51a50064ca45d4b
Juho <juho.carpen@gmail.com>
2021-01-16 15:05:35 +0200

    t4.2. Sovellus jaettu moduuleihin

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -1,27 +1,15 @@
+const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+const mongoUrl = config.MONGODB_URI
+
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 app.use(cors())
 app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
--- osa4/blogilista/models/blog.js
+++ osa4/blogilista/models/blog.js
@@ -1,9 +1,4 @@
 const mongoose = require('mongoose')
-const config = require('../utils/config')
-
-const mongoUrl = config.MONGODB_URI
-
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const blogSchema = mongoose.Schema({
   title: String,
