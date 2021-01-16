ef76841bb134f4c00d9f4d45eaa51c5a17db6c01
Juho <juho.carpen@gmail.com>
2021-01-16 22:10:44 +0200

    HTTP GET request to /api/blogs

---
+++ osa4/blogilista/tests/test_helper.js
@@ -0,0 +1,27 @@
+const Blog = require('../models/blog')
+
+//pohjautuu dummy.test.js tiedoston blogeihin, joista valittu kolme ensimmäistä
+const initialBlogs = [
+  {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+    likes: 7,
+  },
+  {
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+  },
+  {
+    title: "Canonical string reduction",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+    likes: 12,
+  }
+]
+
+module.exports = {
+  initialBlogs
+}
