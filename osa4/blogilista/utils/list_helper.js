1f97793399cceac1fa6b4fcb795d852b45facb9f
Juho <juho.carpen@gmail.com>
2021-01-16 15:53:18 +0200

    t4.4. totalLikes toimii ja on testattu

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  const reducer = (sum, item) => sum + item
+
+  return blogs.length === 0
+    ? 0
+    : blogs
+      .map(blog => blog.likes)
+      .reduce(reducer)
+
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
