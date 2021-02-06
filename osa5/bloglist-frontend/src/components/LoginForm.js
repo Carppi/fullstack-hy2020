3af549bd12d7b63080471bd5f2a2db67c4136a02
Juho <juho.carpen@gmail.com>
2021-02-06 15:54:56 +0200

    5.17. Cypress-testi: Sivu näyttää login-formin

--- osa5/bloglist-frontend/src/components/LoginForm.js
+++ osa5/bloglist-frontend/src/components/LoginForm.js
@@ -21,6 +21,7 @@ const LoginForm = ({ handleSubmit }) => {
         <div>
           username:
           <input
+            id='username'
             type="text"
             value={username}
             name="Username"
@@ -30,13 +31,14 @@ const LoginForm = ({ handleSubmit }) => {
         <div>
           password:
           <input
+            id='password'
             type="password"
             value={password}
             name="Password"
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button id='login-button' type="submit">login</button>
       </form>
     </>
   )
