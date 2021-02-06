ca59e69d7b114a75255bd9b84b2b7d468dff1b8d
Juho <juho.carpen@gmail.com>
2021-02-06 16:18:50 +0200

    5.18. Login testattu Cypressillä

--- osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ osa5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,11 +1,40 @@
-describe('Blog app', function() {
+describe('Blog app', function () {
 
-  beforeEach(function() {
+  beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Teppo Testaaja',
+      username: 'tester',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
+  it('Login form is shown', function () {
     cy.contains('Login to application')
+  })
+
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('tester')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Teppo Testaaja logged in')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('tester')
+      cy.get('#password').type('väärä salasana')
+      cy.get('#login-button').click()
+
+      cy.get('.error')
+        .should('contain', 'wrong username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+
+      cy.get('html').should('not.contain', 'Teppo Testaaja logged in')
+    })
+
   })
 })
