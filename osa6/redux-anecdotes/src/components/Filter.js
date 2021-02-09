588ed358968dfa6913001255ba84fb036249a4b4
Juho <juho.carpen@gmail.com>
2021-02-09 22:32:38 +0200

    Filter toteutettu connectilla

--- osa6/redux-anecdotes/src/components/Filter.js
+++ osa6/redux-anecdotes/src/components/Filter.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { filterChange } from '../reducers/filterReducer'
-import { useDispatch } from 'react-redux'
+import { connect } from 'react-redux'
 
-const Filter = () => {
-  const dispatch = useDispatch()
+const Filter = (props) => {
 
   const handleChange = (event) => {
-    dispatch(filterChange(event.target.value))
+    props.filterChange(event.target.value)
   }
   const style = {
     marginBottom: 10
@@ -19,4 +18,13 @@ const Filter = () => {
   )
 }
 
-export default Filter
+const mapDispatchToProps = {
+  filterChange
+}
+
+const ConnectedFilter = connect(
+  null,
+  mapDispatchToProps
+)(Filter)
+
+export default ConnectedFilter
