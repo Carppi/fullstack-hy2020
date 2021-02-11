d00942525c014844d009f030c5bd98f60beaa613
Juho <juho.carpen@gmail.com>
2021-02-11 23:55:33 +0200

    7.7. Country hook toteutettu

--- osa7/country-hook/src/App.js
+++ osa7/country-hook/src/App.js
@@ -18,12 +18,24 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect()
+  useEffect(() => {
+    if (name !== '') {
+      axios
+        .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+        .then(response => {
+          setCountry({ data: response.data[0], found: true })
+        })
+        .catch(error => {
+          setCountry({ found: false })
+        })
+    }
+  }, [name])
 
   return country
 }
 
 const Country = ({ country }) => {
+
   if (!country) {
     return null
   }
@@ -40,8 +52,8 @@ const Country = ({ country }) => {
     <div>
       <h3>{country.data.name} </h3>
       <div>capital {country.data.capital} </div>
-      <div>population {country.data.population}</div> 
-      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+      <div>population {country.data.population}</div>
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`} />
     </div>
   )
 }
