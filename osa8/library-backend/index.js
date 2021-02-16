bf12e6c7cb603865ebe054db813c05768b4c2d7f
Juho <juho.carpen@gmail.com>
2021-02-16 22:45:59 +0200

    8.1. bookCount ja authorCount kyselyt tehty

--- osa8/library-backend/index.js
+++ osa8/library-backend/index.js
@@ -84,12 +84,31 @@ let books = [
 ]
 
 const typeDefs = gql`
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
+
+  type Book {
+    title: String!
+    published: Int
+    author: String!
+    id: ID!
+    genres: [String]
+  }
+
   type Query {
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
+    bookCount: () => books.length,
+    authorCount: () => authors.length
   }
 }
 
