36443a45442dac18ca08790b6dcf8c0e34a536b6
Juho <juho.carpen@gmail.com>
2021-02-17 22:20:51 +0200

    8.8. Kirjailijoiden näkymä

--- osa8/library-frontend/src/components/Authors.js
+++ osa8/library-frontend/src/components/Authors.js
@@ -1,11 +1,30 @@
   
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors  {
+      name
+      born
+      bookCount
+    }
+  }
+`
 
 const Authors = (props) => {
+
+  const result = useQuery(ALL_AUTHORS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+
   if (!props.show) {
     return null
   }
-  const authors = []
+  const authors = result.data.allAuthors
 
   return (
     <div>
