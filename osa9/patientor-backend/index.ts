473c3243eccb706b3e62a750ffdaacffdb4b5e88
Juho <juho.carpen@gmail.com>
2021-11-08 19:12:27 +0200

    9.9 Backend connected to frontend

--- osa9/patientor-backend/index.ts
+++ osa9/patientor-backend/index.ts
@@ -1,11 +1,15 @@
 import express from 'express';
 // based on course material for flight diary: https://fullstackopen.com/en/part9/typing_the_express_app
 const app = express();
+import cors from 'cors';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
