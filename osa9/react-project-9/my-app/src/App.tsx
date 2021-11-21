ace1f0154b4c0e6edac6ad74fa420d5c17d2d470
Juho <juho.carpen@gmail.com>
2021-11-21 12:15:29 +0200

    9.14 (part 1) React App initiated with typescript

---
+++ osa9/react-project-9/my-app/src/App.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  //TODO Header component
+  //TODO Content component
+  //TODO Total component
+
+  return ( //TODO update references to components
+    <div>
+      <h1>{courseName}</h1>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  );
+};
+
+export default App;
