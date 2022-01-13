43a4e2c3fa34f592cb47929c10baa91ace36cd48
Carppi <juho.carpen@gmail.com>
2022-01-13 15:17:53 +0200

    9.15 New interfaces and small refactoring

--- osa9/react-project-9/my-app/src/App.tsx
+++ osa9/react-project-9/my-app/src/App.tsx
@@ -1,25 +1,84 @@
 import React from 'react';
 const App = () => {
+
+  interface CoursePartBase {
+    name: string;
+    exerciseCount: number;
+    type: string;
+  }
+
+  interface CoursePartBaseWithDescription extends CoursePartBase {
+    description: string;
+  }
+  
+  interface CourseNormalPart extends CoursePartBaseWithDescription {
+    type: "normal";
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+  
+  interface CourseSubmissionPart extends CoursePartBaseWithDescription {
+    type: "submission";
+    exerciseSubmissionLink: string;
+  }
+
+  interface CourseSpecialPart extends CoursePartBaseWithDescription {
+    type: "special";
+    requirements: string[];
+  }
+  
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart | CourseSpecialPart;
+
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal"
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission"
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special"
     }
-  ];
+  ]
 
-  interface CoursePart {
-    name: string;
-    exerciseCount: number;
-  }
+  //HELPER FUNCTIONS:
+
+  /**
+   * Helper function for exhaustive type checking
+   */
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  //COMPONENTS:
 
   //Header component should take care of 1. rendering the name of the course. 
 
@@ -27,15 +86,75 @@ const App = () => {
     <h1>{name}</h1>
   );
 
+  /* Then create a component Part that renders 
+  all attributes of each type of course part. 
+  Use a switch case-based exhaustive type checking! 
+  Use the new component in component Content. */
+
+  const CommonPart = ({part}: {part: CoursePart}) => {
+    return (
+      <div>
+        <h2>
+          {part.name} (type: {part.type}, exercises: {part.exerciseCount})
+        </h2>
+      </div>
+    )
+  }
+
+  const Description = ({description}: {description: string}) => (
+    <i>Description: {description}</i>
+  )
+
+  const Part = ({part}: {part: CoursePart}) => {
+    switch (part.type) {
+      case "normal":
+        return(
+          <div>
+            <CommonPart part={part} />
+            <Description description={part.description}/>
+          </div>
+        );
+
+      case "groupProject":
+        return (
+          <div>
+            <CommonPart part={part} />
+            <p>Group project count: {part.groupProjectCount}</p>
+          </div>
+        );
+
+      case "submission":
+        return(
+          <div>
+            <CommonPart part={part} />
+            <Description description={part.description}/>
+            <p>Exercise submission link: {part.exerciseSubmissionLink}</p>
+          </div>
+        );
+
+      case "special":
+        return(
+          <div>
+            <CommonPart part={part} />
+            <Description description={part.description}/>
+            <div>
+              <p>Requirements: {part.requirements.join(", ")}</p>
+            </div>
+          </div>
+        );
+      
+      default:
+        return assertNever(part);
+    }
+  }
+
   //Content component should 1.render the names of the different parts and 2.the amount of exercises in each part
 
   const Content = ({parts}: { parts: CoursePart[] }): JSX.Element => {
     return (
       <div>
         {parts.map(part => (
-          <p key={part.name}>
-            {part.name} {part.exerciseCount}
-          </p>
+          <Part key={part.name} part={part}/>
         ))}
       </div>
     )
@@ -45,10 +164,10 @@ const App = () => {
 
   const Total = ({parts}: { parts: CoursePart[] }) => {
     return (
-      <p>
+      <h2>
         Number of exercises{" "}
         {parts.reduce((carry, part) => carry + part.exerciseCount, 0)}
-      </p>
+      </h2>
     )
   };
 
